9759f754dc55748cc1a6faffddfb6c8a69f9f12c
kayo <kayohramos@gmail.com>
2023-12-26 18:38:39 -0300

    Desafio 1 completo

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using desafio_praticando_poo.models;
+
+DesafioPOO1 p1 = new DesafioPOO1();
+p1.DadosCompletos();
---
+++ models/DesafioPOO1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio_praticando_poo.models
+{
+    public class DesafioPOO1
+    {
+        private string nome;
+        private int idade;
+
+        //Pessoa
+        public DesafioPOO1(string n, int i)
+    {
+        nome = n;
+        idade = i;
+    }
+
+        public DesafioPOO1()
+        {
+        }
+    //JEITO 1
+    //     public string GetNome(string nome)
+    // {
+    //     return nome;
+    // }
+
+    // public int GetIdade(int idade)
+    // {
+
+    //     return idade;
+    // }
+
+    //JEITO 2
+    public string GetNome()
+    {        
+        string nome = Console.ReadLine();
+
+        return nome;
+    }
+
+    public int GetIdade()
+    {
+        int idade = int.Parse(Console.ReadLine());
+
+        return idade;
+    }
+    
+    public void DadosCompletos()
+    {
+
+    Console.WriteLine($"Nome: {GetNome()}, Idade: {GetIdade()}");
+
+    }
+
+        
+    }
+}
